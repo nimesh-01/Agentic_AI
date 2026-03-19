21882532646d25da7a03f7db10f48a34b39df962
user.email "nimeshsolanki106@gmail.com <nimeshsolanki106@gmail.com>
2026-03-19 15:44:11 +0530

    update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const chatroutes = require('./routes/chat.routes')
 const app = express()
 
 app.use(cors({
-  origin: [ "https://agentic-ai-01.onrender.com","http://localhost:5173"],
+  origin: [ "https://agentic-ai-01.onrender.com","https://agenticais.netlify.app","http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }))
